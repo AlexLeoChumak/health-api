99063e1dceccf670a813670e61208f744cb665d7
AlexLeoChumak <chumak1106@gmail.com>
2024-10-11 12:52:32 +0300

    create DatabaseException

--- src/common/exceptions/database.exception.ts
+++ src/common/exceptions/database.exception.ts
@@ -2,6 +2,6 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class DatabaseException extends HttpException {
   constructor(message: string = 'Database Error') {
-    super(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    super(message, HttpStatus.BAD_REQUEST);
   }
 }
