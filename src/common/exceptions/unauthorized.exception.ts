ce8afda932d29b991aab1f272c4d6ea25d502f3e
AlexLeoChumak <chumak1106@gmail.com>
2024-10-11 12:53:54 +0300

    create UnauthorizedException

--- src/common/exceptions/unauthorized.exception.ts
+++ src/common/exceptions/unauthorized.exception.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class UnauthorizedException extends HttpException {
-  constructor(message: string = 'Unauthorized') {
+  constructor(message: string = 'Неавторизованный доступ') {
     super(message, HttpStatus.UNAUTHORIZED);
   }
 }
