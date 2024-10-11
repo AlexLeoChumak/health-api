8f8c6fa9ecb17861ac988659f8c3b4b26d960b21
AlexLeoChumak <chumak1106@gmail.com>
2024-10-11 12:53:18 +0300

    remove

--- src/common/exceptions/authorization-b2-cloud-storage.exeption.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-export class AuthorizationB2CloudStorageException extends HttpException {
-  constructor(message: string = 'Authorization Error') {
-    super(message, HttpStatus.UNAUTHORIZED);
-  }
-}
--- src/common/exceptions/redis-cashed-token.exception.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-export class RedisCashedTokenException extends HttpException {
-  constructor(message: string = 'Redis Error') {
-    super(message, HttpStatus.INTERNAL_SERVER_ERROR);
-  }
-}
