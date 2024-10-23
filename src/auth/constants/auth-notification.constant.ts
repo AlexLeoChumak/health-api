0dddad533f144d0e691a7d718afa77210d8b09d1
AlexLeoChumak <chumak1106@gmail.com>
2024-10-23 21:04:23 +0300

    create AUTH_NOTIFICATIONS

---
+++ src/auth/constants/auth-notification.constant.ts
@@ -0,0 +1,18 @@
+interface AuthNotificationInterface {
+  REGISTRATION_SUCCESS: string;
+  REGISTRATION_USER_NOT_FOUND_ERROR: string;
+  REGISTRATION_PASSWORD_NOT_FOUND_ERROR: string;
+  LOGIN_INVALID_PHONE_NUMBER_OR_PASSWORD: string;
+  LOGIN_USER_NOT_FOUND_ERROR: string;
+}
+
+export const AUTH_NOTIFICATIONS: AuthNotificationInterface = {
+  REGISTRATION_SUCCESS:
+    'Регистрация прошла успешно! Теперь войдите в свой личный кабинет.',
+  REGISTRATION_USER_NOT_FOUND_ERROR:
+    'Ошибка при регистрации нового пользователя. Попробуйте зарегистрироваться ещё раз.',
+  REGISTRATION_PASSWORD_NOT_FOUND_ERROR:
+    'Ошибка при регистрации. Отсутствует пароль.',
+  LOGIN_INVALID_PHONE_NUMBER_OR_PASSWORD: 'Неверный номер телефона или пароль',
+  LOGIN_USER_NOT_FOUND_ERROR: 'Неверный номер телефона или пароль',
+};
