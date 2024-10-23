cd6fc1bd4cfad0d0c09ca5215f235573b0fa1df1
AlexLeoChumak <chumak1106@gmail.com>
2024-10-23 21:03:11 +0300

    remove import entyties

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,23 +1,8 @@
 import { Logger, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthController } from 'src/auth/auth.controller';
 import { AuthService } from 'src/auth/auth.service';
-import {
-  AddressMedicalInstitutionInfo,
-  DoctorEntity,
-  EducationMedicalWorkerInfo,
-  PlaceWorkInfo,
-} from 'src/auth/entities/doctor.entity';
-import {
-  AddressRegistrationInfo,
-  AddressResidenceInfo,
-  ContactInfo,
-  IdentificationInfo,
-  PatientEntity,
-  PersonalInfo,
-} from 'src/auth/entities/patient.entity';
 import { CommonModule } from 'src/common/common.module';
 import { EntitiesModule } from 'src/entities/entities.module';
 
