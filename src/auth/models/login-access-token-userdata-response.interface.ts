40f62aa6c55bab4b89ab6358fd8d166c5c92b70a
AlexLeoChumak <chumak1106@gmail.com>
2024-10-26 17:50:07 +0300

    change user to filteredUserFields

--- src/auth/models/login-access-token-userdata-response.interface.ts
+++ src/auth/models/login-access-token-userdata-response.interface.ts
@@ -3,5 +3,5 @@ import { PatientEntity } from 'src/auth/entities/patient.entity';
 
 export interface LoginAccessTokenUserDataResponseInterface {
   accessToken: string;
-  user: PatientEntity | DoctorEntity;
+  filteredUserFields: PatientEntity | DoctorEntity;
 }
