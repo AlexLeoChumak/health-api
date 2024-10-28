b4b6694e27983c6070a4cd36d5b2828cf9ad9de2
AlexLeoChumak <chumak1106@gmail.com>
2024-10-28 20:59:23 +0300

    remove Dto's

--- src/auth/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class LoginDto {
-  @IsString()
-  mobilePhoneNumber: string;
-
-  @IsString()
-  password: string;
-}
--- src/auth/models/login-access-token-userdata-response.interface.ts
+++
@@ -1,7 +0,0 @@
-import { DoctorEntity } from 'src/auth/entities/doctor.entity';
-import { PatientEntity } from 'src/auth/entities/patient.entity';
-
-export interface LoginAccessTokenUserDataResponseInterface {
-  accessToken: string;
-  filteredUserFields: PatientEntity | DoctorEntity;
-}
