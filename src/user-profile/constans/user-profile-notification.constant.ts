0df4f396717a2a8b9d3ca1af1a6a1528ec73974f
AlexLeoChumak <chumak1106@gmail.com>
2024-11-13 19:58:04 +0300

    removed   UPDATE_PHOTO_SUCCESS

--- src/user-profile/constans/user-profile-notification.constant.ts
+++ src/user-profile/constans/user-profile-notification.constant.ts
@@ -1,9 +1,7 @@
 interface UserProfileNotificationInterface {
   USER_NOT_FOUND_ERROR: string;
-  UPDATE_PHOTO_SUCCESS: string;
 }
 
 export const USER_PROFILE_NOTIFICATIONS: UserProfileNotificationInterface = {
   USER_NOT_FOUND_ERROR: 'Пользователь не найден',
-  UPDATE_PHOTO_SUCCESS: 'Фото успешно загружено',
 };
