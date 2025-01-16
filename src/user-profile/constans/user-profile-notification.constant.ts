d75bf421f52c087a17e58145eb153d3af9c34084
AlexLeoChumak <chumak1106@gmail.com>
2025-01-16 08:54:14 +0300

    create USER_PROFILE_NOTIFICATIONS

--- src/user-profile/constans/user-profile-notification.constant.ts
+++
@@ -1,7 +0,0 @@
-interface UserProfileNotificationInterface {
-  USER_NOT_FOUND_ERROR: string;
-}
-
-export const USER_PROFILE_NOTIFICATIONS: UserProfileNotificationInterface = {
-  USER_NOT_FOUND_ERROR: 'Пользователь не найден',
-};
