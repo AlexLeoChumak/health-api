6d9f40c831b7f52c68a7fa6a6346ef03fca276a4
AlexLeoChumak <chumak1106@gmail.com>
2025-01-16 08:37:26 +0300

    add providers: [     RegistrationService,     LoginService,     AccessRefreshTokenService,     SensitiveFieldsUserService,     Logger,   ],

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,20 +2,29 @@ import { Logger, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 
 import { AuthController } from 'src/auth/auth.controller';
-import { AuthService } from 'src/auth/auth.service';
+import { LoginService } from 'src/auth/services/login.service';
+import { RegistrationService } from 'src/auth/services/registration.service';
 import { CommonModule } from 'src/common/common.module';
 import { EntitiesModule } from 'src/entities/entities.module';
+import { AccessRefreshTokenService } from './services/access-refresh-token.service';
+import { SensitiveFieldsUserService } from 'src/auth/services/sensitive-fields-user.service';
 
 @Module({
   imports: [
-    EntitiesModule,
     CommonModule,
+    EntitiesModule,
     JwtModule.register({
       secret: 'interstellar',
       signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, Logger],
+  providers: [
+    RegistrationService,
+    LoginService,
+    AccessRefreshTokenService,
+    SensitiveFieldsUserService,
+    Logger,
+  ],
 })
 export class AuthModule {}
