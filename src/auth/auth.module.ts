016dfe50599deffd610903c79273fbc18a910f00
AlexLeoChumak <chumak1106@gmail.com>
2025-03-02 22:10:40 +0300

    add RepositoriesModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,18 +5,20 @@ import { AuthController } from 'src/auth/auth.controller';
 import { LoginService } from 'src/auth/services/login.service';
 import { RegistrationService } from 'src/auth/services/registration.service';
 import { CommonModule } from 'src/common/common.module';
-import { EntitiesModule } from 'src/entities/entities.module';
 import { AccessRefreshTokenService } from './services/access-refresh-token.service';
 import { SensitiveFieldsUserService } from 'src/auth/services/sensitive-fields-user.service';
+import { PasswordService } from './services/password/password.service';
+import { RepositoriesModule } from 'src/repositories/repositories.module';
 
 @Module({
   imports: [
     CommonModule,
-    EntitiesModule,
+    RepositoriesModule,
     JwtModule.register({
       secret: 'interstellar',
       signOptions: { expiresIn: '1h' },
     }),
+    RepositoriesModule,
   ],
   controllers: [AuthController],
   providers: [
@@ -24,6 +26,7 @@ import { SensitiveFieldsUserService } from 'src/auth/services/sensitive-fields-u
     LoginService,
     AccessRefreshTokenService,
     SensitiveFieldsUserService,
+    PasswordService,
     Logger,
   ],
 })
