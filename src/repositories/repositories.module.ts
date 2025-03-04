a35353e6175d71cbf81e4bee422abe9cac513a94
AlexLeoChumak <chumak1106@gmail.com>
2025-03-04 22:37:47 +0300

    add entities

--- src/repositories/repositories.module.ts
+++ src/repositories/repositories.module.ts
@@ -1,42 +1,53 @@
 import { Logger, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DoctorEntityRepository } from 'src/repositories/doctor-entity.repository';
+import { PatientEntityRepository } from 'src/repositories/patient-entity.repository';
+import { PersonalInfoEntityRepository } from 'src/repositories/personal-info-entity.repository';
 import {
-  AddressMedicalInstitutionInfo,
   DoctorEntity,
-  EducationMedicalWorkerInfo,
-  PlaceWorkInfo,
+  AddressMedicalInstitutionInfoEntity,
+  EducationMedicalWorkerInfoEntity,
+  PlaceWorkInfoEntity,
 } from 'src/repositories/entities/doctor.entity';
 import {
-  AddressRegistrationInfo,
-  AddressResidenceInfo,
-  ContactInfo,
-  IdentificationBelarusCitizenInfo,
-  IdentificationForeignCitizenInfo,
-  MobilePhoneNumberPasswordInfo,
   PatientEntity,
-  PersonalInfo,
+  AddressRegistrationInfoEntity,
+  AddressResidenceInfoEntity,
+  ContactInfoEntity,
+  IdentificationBelarusCitizenInfoEntity,
+  IdentificationForeignCitizenInfoEntity,
+  PersonalInfoEntity,
+  MobilePhoneNumberPasswordInfoEntity,
 } from 'src/repositories/entities/patient.entity';
-import { PatientEntityRepository } from 'src/repositories/patient-entity.repository';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       PatientEntity,
       DoctorEntity,
-      AddressRegistrationInfo,
-      AddressResidenceInfo,
-      ContactInfo,
-      IdentificationBelarusCitizenInfo,
-      IdentificationForeignCitizenInfo,
-      PersonalInfo,
-      AddressMedicalInstitutionInfo,
-      EducationMedicalWorkerInfo,
-      PlaceWorkInfo,
-      MobilePhoneNumberPasswordInfo,
+      AddressRegistrationInfoEntity,
+      AddressResidenceInfoEntity,
+      ContactInfoEntity,
+      IdentificationBelarusCitizenInfoEntity,
+      IdentificationForeignCitizenInfoEntity,
+      PersonalInfoEntity,
+      AddressMedicalInstitutionInfoEntity,
+      EducationMedicalWorkerInfoEntity,
+      PlaceWorkInfoEntity,
+      MobilePhoneNumberPasswordInfoEntity,
     ]),
   ],
-  providers: [PatientEntityRepository, DoctorEntityRepository, Logger],
-  exports: [TypeOrmModule, PatientEntityRepository, DoctorEntityRepository],
+  providers: [
+    PatientEntityRepository,
+    DoctorEntityRepository,
+    PersonalInfoEntityRepository,
+    Logger,
+  ],
+  exports: [
+    TypeOrmModule,
+    PatientEntityRepository,
+    DoctorEntityRepository,
+    PersonalInfoEntityRepository,
+  ],
 })
 export class RepositoriesModule {}
