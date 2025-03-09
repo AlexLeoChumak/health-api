a4be2cfa0465ee8f0dab2ef5afc2a069f5560eea
AlexLeoChumak <chumak1106@gmail.com>
2025-03-09 16:51:18 +0300

    add @IsString() @IsNotEmpty() userRole: UserRoleType;

--- src/modules/user-profile/dto/update-password.dto.ts
+++ src/modules/user-profile/dto/update-password.dto.ts
@@ -1,8 +1,9 @@
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { UserRoleType } from 'src/modules/user-profile/models/user-role.type';
 
 export class UpdatePasswordDto {
   @IsString() @IsNotEmpty() userId: string;
-  @IsString() @IsNotEmpty() userRole: 'doctor' | 'patient';
+  @IsString() @IsNotEmpty() userRole: UserRoleType;
   @IsString() @IsNotEmpty() oldPassword: string;
   @IsString() @IsNotEmpty() @MinLength(8) newPassword: string;
   @IsString() @IsNotEmpty() @MinLength(8) newPasswordConfirmation: string;
