b541eca9e037ab8d3da77716f4ae196f674ee39c
AlexLeoChumak <chumak1106@gmail.com>
2025-03-09 16:54:12 +0300

    remove space

--- src/repositories/doctor-entity.repository.ts
+++ src/repositories/doctor-entity.repository.ts
@@ -1,7 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-
 import { DoctorEntity } from 'src/repositories/entities/doctor.entity';
 import { BaseEntityRepository } from 'src/repositories/base-entity.repository';
 
