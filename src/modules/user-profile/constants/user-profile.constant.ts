8d5cf7c9a24a48d86d8d326aa74a74ff3b1f948c
AlexLeoChumak <chumak1106@gmail.com>
2025-03-13 13:17:36 +0300

    add INFO_GROUP_UPDATED_SUCCESSFULLY: string;   INFO_GROUP_NOT_FOUND: string;   REPOSITORY_NOT_FOUND: string;

--- src/modules/user-profile/constants/user-profile.constant.ts
+++ src/modules/user-profile/constants/user-profile.constant.ts
@@ -2,10 +2,16 @@ export interface UserProfileConstantInterface {
   CURRENT_PASSWORD_INVALID: string;
   NEW_PASSWORDS_NO_MATCH: string;
   PASSWORD_UPDATED_SUCCESSFULLY: string;
+  INFO_GROUP_UPDATED_SUCCESSFULLY: string;
+  INFO_GROUP_NOT_FOUND: string;
+  REPOSITORY_NOT_FOUND: string;
 }
 
 export const USER_PROFILE_CONSTANT: UserProfileConstantInterface = {
   CURRENT_PASSWORD_INVALID: 'Текущий пароль неверен',
   NEW_PASSWORDS_NO_MATCH: 'Новый пароль и его подтверждение не совпадают',
   PASSWORD_UPDATED_SUCCESSFULLY: 'Пароль успешно обновлён',
+  INFO_GROUP_UPDATED_SUCCESSFULLY: 'Информация успешно обновлёна',
+  INFO_GROUP_NOT_FOUND: 'Информационная группа не найдена',
+  REPOSITORY_NOT_FOUND: 'Репозиторий не найден',
 };
