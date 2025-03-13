cdb283a5922ceb4bee582e2b9deb0881364fd40d
AlexLeoChumak <chumak1106@gmail.com>
2025-03-13 13:19:16 +0300

    create CloudStorageModule

---
+++ src/shared/modules/cloud-storage/cloud-storage.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { CloudStorageController } from 'src/shared/modules/cloud-storage/cloud-storage.controller';
+import { CloudStorageService } from 'src/shared/modules/cloud-storage/cloud-storage.service';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [CloudStorageController],
+  providers: [CloudStorageService],
+  exports: [CloudStorageService],
+})
+export class CloudStorageModule {}
