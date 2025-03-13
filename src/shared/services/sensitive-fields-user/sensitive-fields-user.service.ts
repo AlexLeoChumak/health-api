a71dcdd33ff8ed346295018eb16f75f50b96864b
AlexLeoChumak <chumak1106@gmail.com>
2025-03-13 13:19:52 +0300

    create SensitiveFieldsUserService

---
+++ src/shared/services/sensitive-fields-user/sensitive-fields-user.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { DoctorEntity } from 'src/repositories/entities/doctor.entity';
+import { PatientEntity } from 'src/repositories/entities/patient.entity';
+import {
+  PatientBaseResponseDto,
+  DoctorBaseResponseDto,
+} from 'src/modules/auth/dto/user-response.dto';
+import { SHARED_CONSTANT } from 'src/common/constants/shared.constant';
+
+@Injectable()
+export class SensitiveFieldsUserService {
+  public removeSensitiveFieldsFromUser(
+    user: PatientEntity | DoctorEntity,
+  ): PatientBaseResponseDto | DoctorBaseResponseDto {
+    if (typeof user !== 'object') {
+      throw new InternalServerErrorException(
+        SHARED_CONSTANT.USER_NOT_FOUND_ERROR,
+      );
+    }
+
+    const filteredObj = {};
+    const skipFields = ['hashedPassword', 'refreshToken'];
+
+    for (const [field, value] of Object.entries(user)) {
+      if (skipFields.includes(field) || value === undefined) {
+        continue;
+      }
+
+      if (typeof value === 'object' && value !== null) {
+        const nested = this.removeSensitiveFieldsFromUser(value);
+
+        if (nested && Object.keys(nested).length > 0) {
+          filteredObj[field] = nested;
+        }
+      } else {
+        filteredObj[field] = value;
+      }
+    }
+
+    return filteredObj as PatientBaseResponseDto | DoctorBaseResponseDto;
+  }
+}
