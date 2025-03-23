2a1268097787f8152ce3cbf38da35c5d60cfdc60
AlexLeoChumak <chumak1106@gmail.com>
2025-03-23 12:11:26 +0300

    add APPLICATION_ERROR: string;   FORBIDDEN_EXCEPTION: string;

--- src/common/constants/shared.constant.ts
+++ src/common/constants/shared.constant.ts
@@ -2,10 +2,14 @@ export interface SharedConstantInterface {
   USER_NOT_FOUND_ERROR: string;
   AUTHORIZATION_HEADER_MISSING: string;
   TOKEN_INVALID_OR_EXPIRED: string;
+  APPLICATION_ERROR: string;
+  FORBIDDEN_EXCEPTION: string;
 }
 
 export const SHARED_CONSTANT: SharedConstantInterface = {
   USER_NOT_FOUND_ERROR: 'Пользователь не найден',
   AUTHORIZATION_HEADER_MISSING: 'Отсутствует заголовок авторизации',
   TOKEN_INVALID_OR_EXPIRED: 'Недействительный или просроченный токен',
+  APPLICATION_ERROR: 'Ошибка приложения. Пожалуйста, обновите страницу',
+  FORBIDDEN_EXCEPTION: 'Доступ запрещен',
 };
