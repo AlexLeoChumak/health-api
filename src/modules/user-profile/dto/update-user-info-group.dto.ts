6d41778687137043e957c4b6351f440af580d915
AlexLeoChumak <chumak1106@gmail.com>
2025-03-23 12:13:04 +0300

    add currentUserPhotoData: CurrentUserPhotoDataInterface;

--- src/modules/user-profile/dto/update-user-info-group.dto.ts
+++ src/modules/user-profile/dto/update-user-info-group.dto.ts
@@ -2,6 +2,7 @@ import { Type } from 'class-transformer';
 import { IsString, IsNotEmpty, IsObject } from 'class-validator';
 import { UpdateUserInfoGroupType } from 'src/modules/user-profile/models/update-user-info-group.type';
 import { UserRoleType } from 'src/common/models/user-role.type';
+import { CurrentUserPhotoDataInterface } from 'src/modules/user-profile/models/current-user-photo-data.interface';
 
 export class UpdateUserInfoGroupDto {
   @IsString() @IsNotEmpty() userId: string;
@@ -10,4 +11,8 @@ export class UpdateUserInfoGroupDto {
   @IsObject()
   @Type(() => Object)
   updateInfoGroup: UpdateUserInfoGroupType;
+
+  @IsObject()
+  @Type(() => Object)
+  currentUserPhotoData: CurrentUserPhotoDataInterface;
 }
