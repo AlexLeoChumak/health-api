5effa2876a4f9e493a2c363502bc04b84b29aaf9
AlexLeoChumak <chumak1106@gmail.com>
2025-03-24 10:22:33 +0300

    create REPOSITORY_CONSTANT

---
+++ src/repositories/constants/repository.constant.ts
@@ -0,0 +1,7 @@
+export interface RepositoryConstantInterface {
+  REPOSITORY_MISSING: string;
+}
+
+export const REPOSITORY_CONSTANT: RepositoryConstantInterface = {
+  REPOSITORY_MISSING: 'Репозиторий не найден',
+};
