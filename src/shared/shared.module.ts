2d474eeddaf028d76c1882bf8de3da6e7a9622bb
AlexLeoChumak <chumak1106@gmail.com>
2025-04-08 21:09:21 +0300

    add RepositoriesModule

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { RepositoriesModule } from 'src/repositories/repositories.module';
 import { CloudStorageModule } from 'src/shared/modules/cloud-storage/cloud-storage.module';
 import { JwtConfigModule } from 'src/shared/modules/jwt-config/jwt-config.module';
 import { SensitiveFieldsUserService } from 'src/shared/services/sensitive-fields-user/sensitive-fields-user.service';
 
 @Module({
   imports: [CloudStorageModule, JwtConfigModule],
-  providers: [SensitiveFieldsUserService],
+  providers: [RepositoriesModule, SensitiveFieldsUserService],
   exports: [CloudStorageModule, JwtConfigModule, SensitiveFieldsUserService],
 })
 export class SharedModule {}
